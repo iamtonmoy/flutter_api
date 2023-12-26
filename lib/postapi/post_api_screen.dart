c0daf771e0f4041130e406e192811d736171d39e
tonmoy_m <tonmoy@bitmascot.com>
2023-12-26 15:41:42 +0600

    list,login,signup,signin

---
+++ lib/postapi/post_api_screen.dart
@@ -0,0 +1,44 @@
+import 'package:apitutorials/getapi/get_api_screen.dart';
+import 'package:apitutorials/signup.dart';
+import 'package:apitutorials/upload_image.dart';
+import 'package:flutter/material.dart';
+
+class PostApiScreen extends StatefulWidget {
+  const PostApiScreen({Key? key}) : super(key: key);
+
+  @override
+  _PostApiScreenState createState() => _PostApiScreenState();
+}
+
+class _PostApiScreenState extends State<PostApiScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('My Api Learning'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          children: [
+            ReusableWidget(
+              title: 'Login & SignUp',
+              subTitle: 'Login and sign up api integration using open source api',
+              onPress: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpScreen()));
+              },
+            ),
+            ReusableWidget(
+              title: 'Upload image to server',
+              subTitle: 'Upload image to server using image path(url)',
+              onPress: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => UploadImageScreen()));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
